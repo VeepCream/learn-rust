15cac83bbad2cf46eede50c25527c6e828e08ccb
Veerapat 'vee' Preechapant <veerapat@conicle.com>
2025-10-03 18:40:55 +0700

    Remove generated target files and cache for Rust project

--- target/debug/deps/learn_rust-41bd24cbef739182.d
+++
@@ -1,5 +0,0 @@
-/Users/conicle/Rust/learn-rust/target/debug/deps/learn_rust-41bd24cbef739182: src/main.rs
-
-/Users/conicle/Rust/learn-rust/target/debug/deps/learn_rust-41bd24cbef739182.d: src/main.rs
-
-src/main.rs:
--- target/debug/learn-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/conicle/Rust/learn-rust/target/debug/learn-rust: /Users/conicle/Rust/learn-rust/src/main.rs
